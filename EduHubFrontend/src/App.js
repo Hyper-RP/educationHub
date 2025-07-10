9e7f0fc5fe4cc2b793de7b0dba0b4434259d6d93
Hyper-RP <rp302084@gmail.com>
2025-07-11 00:30:32 +0530

    verify user register feature with backend

--- EduHubFrontend/src/App.js
+++ EduHubFrontend/src/App.js
@@ -9,24 +9,24 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/user/allUsers";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [userName, setUserName] = useState("");
-  const [userPass, setUserPass] = useState();
-  const [userId,setUserId]=useState();
+  // const [users, setUsers] = useState([]);
+  // const [userName, setUserName] = useState("");
+  // const [userPass, setUserPass] = useState();
+  // const [userId,setUserId]=useState();
 
-  useEffect(() => {
-    axios
-      .get(API_URL)
-      .then((res) => setUsers(res.data))
-      .catch((err) => "springBoot se data nahi aaya hai");
-  },[]);
+  // useEffect(() => {
+  //   axios
+  //     .get(API_URL)
+  //     .then((res) => setUsers(res.data))
+  //     .catch((err) => "springBoot se data nahi aaya hai");
+  // },[]);
 
-  const handleAddUser = () => {
-    axios
-      .post("http://localhost:8080/user/addUser", {userId:userId, userName: userName, userPassword: userPass })
-      .then((res) => setUsers([...users, res.data]))
-      .catch((err) => console.error("user add nahi hua"));
-  };
+  // const handleAddUser = () => {
+  //   axios
+  //     .post("http://localhost:8080/user/addUser", {userId:userId, userName: userName, userPassword: userPass })
+  //     .then((res) => setUsers([...users, res.data]))
+  //     .catch((err) => console.error("user add nahi hua"));
+  // };
 
   return (
     <Router>
--- EduHubFrontend/src/pages/UserSignIn.jsx
+++ EduHubFrontend/src/pages/UserSignIn.jsx
@@ -1,27 +1,89 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
+import axios from "axios";
 import { Box, Typography, Container, TextField, Button } from "@mui/material";
 import { motion } from "framer-motion";
+import { toast } from "react-toastify";
 
+const PASSWORD_REGEX =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*{}[\]|:;"'<>.?/~`_=])(?!.*[ ,()\-\+]).{8,}$/;
+
+const GMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 const UserSignIn = () => {
   const [formData, setFormData] = useState({
-    name:"",
-    email:"",
+    name: "",
+    email: "",
     password: "",
     confirmPassword: "",
   });
 
-  function submitHandler(e) {
+  const [pwd, setPwd] = useState("");
+  const [errMessage, setErrMessage] = useState({
+    mailError: "",
+    passError: "",
+    confirmPasswordError: "",
+  });
+
+  const signInHandler = (e) => {
     e.preventDefault();
     if (formData.password === formData.confirmPassword) {
+      axios
+        .post("http://localhost:8080/students/RegisterStudent", {
+          name: formData.name,
+          email: formData.email,
+          password: formData.password,
+        })
+        .then(() => toast.success("Registered"))
+        .catch(() =>
+          toast.error(
+            formData.email + " this mail already exits, choose another mail"
+          )
+        );
+
       setFormData({
         name: "",
         email: "",
         password: "",
-        confirmPassword:""
+        confirmPassword: "",
       });
     }
+  };
+
+  function isPasswordValid(pw) {
+    return PASSWORD_REGEX.test(pw);
   }
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (!isPasswordValid(pwd) && formData.password !== "") {
+        setErrMessage((prev) => ({
+          ...prev,
+          passError: "Password must contain letters, numbers, a symbol",
+        }));
+      } else {
+        setErrMessage((prev) => ({
+          ...prev,
+          passError: "",
+        }));
+      }
+    }, 1000);
+  }, [formData.password]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (!GMAIL_REGEX.test(formData.email) && formData.email !== "") {
+        setErrMessage((prev) => ({
+          ...prev,
+          mailError: "Enter a valid mail address",
+        }));
+      } else {
+        setErrMessage((prev) => ({
+          ...prev,
+          mailError: "",
+        }));
+      }
+    }, 1000);
+  }, [formData.email]);
 
   console.log("form data : " + formData.name);
 
@@ -44,7 +106,12 @@ const UserSignIn = () => {
             <Typography component="h1" variant="h5">
               User Sign In
             </Typography>
-            <Box component="form" noValidate sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              sx={{ mt: 1 }}
+              onSubmit={(e) => signInHandler(e)}
+            >
               <TextField
                 value={formData.name}
                 onChange={(e) =>
@@ -69,27 +136,33 @@ const UserSignIn = () => {
                 onChange={(e) =>
                   setFormData((prev) => ({
                     ...prev,
-                    email: e.target.value,
+                    email: e.target.value.trim(),
                   }))
                 }
                 margin="normal"
                 required
                 fullWidth
                 id="email"
-                label="Email Address"
+                label="eduhub@gmail.com"
                 name="email"
                 type="email"
                 autoComplete="current-email"
                 autoFocus
               />
+              {formData.email !== "" ? (
+                <span style={{ color: "red" }}>{errMessage.mailError}</span>
+              ) : (
+                ""
+              )}
               <TextField
                 value={formData.password}
-                onChange={(e) =>
+                onChange={(e) => {
                   setFormData((prev) => ({
                     ...prev,
                     password: e.target.value,
-                  }))
-                }
+                  }));
+                  setPwd(e.target.value);
+                }}
                 margin="normal"
                 required
                 fullWidth
@@ -99,6 +172,11 @@ const UserSignIn = () => {
                 id="password"
                 autoComplete="current-password"
               />
+              {formData.password !== "" ? (
+                <span style={{ color: "red" }}>{errMessage.passError}</span>
+              ) : (
+                ""
+              )}
 
               <TextField
                 value={formData.confirmPassword}
@@ -118,13 +196,12 @@ const UserSignIn = () => {
               />
 
               <Button
-                onClick={(e) => submitHandler(e)}
                 type="submit"
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                Sign In
+                Register
               </Button>
             </Box>
           </Box>
